5d9de496ff14586feb96b2c1d6a1b39994d26e87
Rachel Shim <cs899@cornell.edu>
2018-10-23 20:23:29 -0400

    semi decent button layout

---
+++ gui/gui.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+
+from tkinter import *
+import numpy as np
+
+class GUI:
+
+	def callback(self):
+		# temp
+		print("fuck")
+
+	def __init__(self, master):
+		self.tiles = []
+		self.master = master
+		self.frame = Frame(master)
+
+		Grid.rowconfigure(master, 0, weight=1)
+		Grid.columnconfigure(master, 0, weight=1)
+		self.frame.grid(row=0, column=0)
+
+		count = 1
+
+		# board setup
+		for i in range(10):
+			Grid.rowconfigure(self.frame, i, weight=1)
+			for j in range(10):
+				Grid.columnconfigure(self.frame, j, weight=1)
+				tile = Button(self.frame)
+				tile.config(height=40,
+							width=40,
+							command=self.callback,
+							text=str(count)) #change this line
+				count += 1 #change this line
+				tile.grid(row=i, column=j)
+				self.tiles.append(tile)
+
+	def update(self):
+		pass # TODO
+
+
+def main():
+	window = Tk()
+	window.title("Stratego")
+	window.geometry("500x500")
+	window.resizable(0, 0)
+	gui = GUI(window)
+
+	window.mainloop()
+
+
+if __name__ == "__main__":
+	main()
