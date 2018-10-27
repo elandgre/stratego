4fb417033c80890829a337e09fbad8d327fb0267
Sheetal Athrey <sheetalathrey@dhcp-rhodes-2092.eduroam.cornell.edu>
2018-10-27 15:30:15 -0400

    init in progress

--- gui/gui.py
+++ gui/gui.py
@@ -2,6 +2,9 @@
 
 from tkinter import *
 import numpy as np
+import game.engine
+import game.constants
+from engine import Engine
 
 class GUI:
 
@@ -18,8 +21,18 @@ class GUI:
 		Grid.columnconfigure(master, 0, weight=1)
 		self.frame.grid(row=0, column=0)
 
-		count = 1
 
+		e = Engine()
+		#e.run()
+		#count = 1
+		#state1 = e.player1.get_starting_state()
+		state2 = e.player2.get_starting_state()
+		#if(e.board.add_player(state1,1)):
+		#	board = e.board.get_player_view(1)
+		added_player = e.board.add_player(state2, 2)
+		print(added_player)
+        if added_player:
+           	board = e.board.get_player_view(2)
 		# board setup
 		for i in range(10):
 			Grid.rowconfigure(self.frame, i, weight=1)
@@ -28,15 +41,23 @@ class GUI:
 				tile = Button(self.frame)
 				tile.config(height=40,
 							width=40,
+#<<<<<<< Updated upstream
 							command=self.callback,
-							text=str(count)) #change this line
-				count += 1 #change this line
+							text=board[i][j]) #change this line
+				#count += 1 #change this line
+#=======
+#							bg="grey",
+#							fg="black",
+#							highlightcolor="green",
+#							activebackground="blue",
+#							text = "")
+#				tile.bind("<Button>", self.callback)
+#>>>>>>> Stashed changes
 				tile.grid(row=i, column=j)
 				self.tiles.append(tile)
 
 	def update(self):
-		pass # TODO
-
+		pass
 
 def main():
 	window = Tk()
