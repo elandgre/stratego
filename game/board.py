2b7988863484d1cae99343f3bcf523fb4e8c933c
Rachel Shim <cs899@cornell.edu>
2018-12-08 02:13:34 -0500

    added active fill, player view?? needs to be tested

--- game/board.py
+++ game/board.py
@@ -306,15 +306,15 @@ class Board():
         new_board = np.zeros((10,10),dtype=int)
         if player == 1:
             for x,y in self.player1_piece_positions:
-                new_board[x][y] = piece_diplay_map[piece_names[self.board[x][y]]]
+                new_board[x][y] = 200 + piece_diplay_map[piece_names[self.board[x][y]]]
             for x,y in self.player2_piece_positions:
-                new_board[x][y] = piece_diplay_map[pieces.HIDDEN.value]
+                new_board[x][y] = 300 + piece_diplay_map[pieces.HIDDEN.value]
             new_board = np.rot90(np.rot90(new_board))
         else:
             for x,y in self.player2_piece_positions:
-                new_board[x][y] = piece_diplay_map[piece_names[self.board[x][y] - self.player2_offset]]
+                new_board[x][y] = 300 + piece_diplay_map[piece_names[self.board[x][y] - self.player2_offset]]
             for x,y in self.player1_piece_positions:
-                new_board[x][y] = piece_diplay_map[pieces.HIDDEN.value]
+                new_board[x][y] = 200 + piece_diplay_map[pieces.HIDDEN.value]
 
         rows = [4,5]
         columns = [2,3,6,7]
